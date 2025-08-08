a6ee047d84fd85c68552cc441d8640f653fb77fa
SeungHun333 <yui332@naver.com>
2025-08-08 17:21:02 +0900

    초기 설정

---
+++ src/main/java/com/hun/sudalmarket/SudalMarketApplication.java
@@ -0,0 +1,13 @@
+package com.hun.sudalmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudalMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudalMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hun/sudalmarket/SudalMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hun.sudalmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudalMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
